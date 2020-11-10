7a86ecca7546b1bafb284712d268854b847d81b9
Har Dhillon <har7173@gmail.com>
2020-11-10 14:57:27 +0000

    delete method created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,9 +18,30 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  private
-
-  def task_params
-    params.require(:task).permit(:title, :details)
+  def edit
+    @task = Task.find(params[:id])
   end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+end
+
+# PRIVATE!
+private
+
+def task_params
+  # turning check box values into boolean
+  params[:task][:completed] = ( params[:task][:completed] == "1" )
+  params.require(:task).permit(:title, :details, :completed)
 end
