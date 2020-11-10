22ef0890d02050787dcb099ece6722c1e5af45cc
Har Dhillon <har7173@gmail.com>
2020-11-10 12:37:02 +0000

    created adding tasks option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,5 +8,19 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
